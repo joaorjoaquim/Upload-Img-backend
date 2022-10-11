b38fecdf9f94f43bb2677a0eeea6562b76e02c49
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-10-11 13:49:56 -0300

    nodemon and server correction

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+    return res.send('Hello Word');
+});
 
 app.listen(3000);
