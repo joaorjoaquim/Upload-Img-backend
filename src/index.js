7bc18e818712d7e0a5cf0dfe34d8d0170d8a6714
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-10-11 18:37:24 -0300

    creating delete and fixing url with local storage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -10,6 +13,10 @@ mongoose.Promise = global.Promise;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(
+    '/files',
+    express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+);
 
 app.use(require('./routes'));
 app.listen(3000);
