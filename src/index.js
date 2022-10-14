38fa61de6c97795ba5600b30b7f2994669878df4
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-10-14 17:01:31 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect(process.env.MONGO_URL);
 mongoose.Promise = global.Promise;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
